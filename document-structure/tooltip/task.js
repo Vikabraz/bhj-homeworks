92149a1f6dbae2234979641085706a53576d4291
Viktoriya Buzmakova <vik-po4ta@mail.ru>
2023-10-28 21:38:45 +0300

    tooltip2

--- document-structure/tooltip/task.js
+++ document-structure/tooltip/task.js
@@ -1,14 +1,15 @@
 const tooltips = document.querySelectorAll(".has-tooltip");
+const toolText = document.createElement("div");
 
 tooltips.forEach((tooltips) => {
   tooltips.addEventListener("click", function (e) {
     e.preventDefault();
     const title = this.getAttribute("title");
-
-    const toolText = document.createElement("div");
     toolText.classList.add("tooltip_active", "tooltip");
+
     toolText.innerText = title;
-    this.insertAdjacentElement("afterend", toolText);
+
+    tooltips.insertAdjacentElement("afterend", toolText);
 
     const tooltipPosition = this.getBoundingClientRect();
     toolText.style.top = tooltipPosition.top + 20 + "px";
